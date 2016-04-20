b444da42da7e2185f20947e649472ef0b3ce6af4
Tim Lancina <tim@ionic.io>
2016-04-20 15:48:23 -0500

    feat(scripts-copy): add es6-shim by default
    Related to
    https://github.com/driftyco/ionic-conference-app/commit/f229cfae822c63fc24489f2e4ee392500ee3e5f4.

--- scripts-copy/index.js
+++ scripts-copy/index.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
 
+var defaultSrc = [
+  'node_modules/es6-shim/es6-shim.min.js',
+  'node_modules/angular2/bundles/angular2-polyfills.js'
+];
+
 module.exports = function(options) {
-  options.src = options.src || 'node_modules/angular2/bundles/angular2-polyfills.js';
+  options.src = options.src || defaultSrc;
   options.dest = options.dest || 'www/build/js';
 
   return gulp.src(options.src)
