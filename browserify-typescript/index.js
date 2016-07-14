4422afcabdcaa94dc70b629e207f060d51ef0dde
Dan Bucholtz <dan.bucholtz@gmail.com>
2016-07-14 12:24:23 -0500

    update package.json

--- browserify-typescript/index.js
+++ browserify-typescript/index.js
@@ -12,7 +12,7 @@ var gulp = require('gulp'),
 
 var defaultOptions = {
   watch: false,
-  src: ['./app/app.ts', './typings/index.d.ts', './node_modules/@types/node-uuid/index.d.ts'],
+  src: ['./app/app.ts', './typings/index.d.ts'],
   outputPath: 'www/build/js/',
   outputFile: 'app.bundle.js',
   minify: false,
